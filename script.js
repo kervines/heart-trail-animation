d8cf25511ddabfa41dd33ce04a6061bd9459294d
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-11-11 00:38:38 +0000

    update project modification

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const bodyEl = document.querySelector('body');
-const divMouseEl = document.querySelector('.mouse');
+const mouseEl = document.querySelector('.mouse');
+const divMouseEl = document.querySelectorAll('.mouse');
 
+const createElPrincipal = (element, x, y) => {
+  const divEl = document.createElement('span');
+  divEl.classList.add('mouse', 'principal');
+  divEl.style.left = `${x}px`;
+  divEl.style.top = `${y}px`;
+  element.appendChild(divEl);
+};
+
+const createElMiddle = (element, x, y) => {
+  const divEl = document.createElement('span');
+  divEl.classList.add('mouse', 'principal', 'animation--middle');
+  divEl.style.left = `${x}px`;
+  divEl.style.top = `${y}px`;
+  element.appendChild(divEl);
+};
+
+const createElend = (element, x, y) => {
+  const divEl = document.createElement('span');
+  divEl.classList.add('mouse', 'principal', 'animation--end');
+  divEl.style.left = `${x}px`;
+  divEl.style.top = `${y}px`;
+  element.appendChild(divEl);
+};
 document.addEventListener('mousemove', (event) => {
   const x = event.clientX;
   const y = event.clientY;
 
-  divMouseEl.style.left = `${x}px`;
-  divMouseEl.style.top = `${y}px`;
+  createElPrincipal(bodyEl, x, y);
+  createElMiddle(bodyEl, x, y);
+  createElend(bodyEl, x, y);
 });
