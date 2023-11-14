a6671c2592068aa506667b3a844e36ec33a3df8b
Kervin Espirito Santo <57775586+kervines@users.noreply.github.com>
2023-11-14 00:41:04 +0000

    update project + my modification

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
 const bodyEl = document.querySelector('body');
-const mouseEl = document.querySelector('.mouse');
-const divMouseEl = document.querySelectorAll('.mouse');
 
-const createElPrincipal = (element, x, y) => {
-  const divEl = document.createElement('span');
-  divEl.classList.add('mouse', 'principal');
-  element.appendChild(divEl);
-  const height = divEl.clientHeight;
-  divEl.style.left = `${x - height / 2}px`;
-  divEl.style.top = `${y - height / 2}px`;
+const createElementTrail = (element, x, y) => {
+  const spanEl = document.createElement('span');
+  const size = Math.random() * 100;
+  spanEl.style.width = `${size}px`;
+  spanEl.style.height = `${size}px`;
+
+  element.appendChild(spanEl);
+  spanEl.style.left = `${x}px`;
+  spanEl.style.top = `${y}px`;
+
+  const colorRandom = Math.round(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, 0);
+
+  spanEl.style.backgroundColor = `#${colorRandom}`;
+  spanEl.style.backgroundBlendMode = 'multiply';
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
 };
 
-const createElMiddle = (element, x, y) => {
-  const divEl = document.createElement('span');
-
-  divEl.classList.add('mouse', 'principal', 'animation--middle');
-  element.appendChild(divEl);
-  const height = divEl.clientHeight;
-  divEl.style.left = `${x - height / 2}px`;
-  divEl.style.top = `${y - height / 2}px`;
-};
-
-const createElend = (element, x, y) => {
-  const divEl = document.createElement('span');
-  divEl.classList.add('mouse', 'principal', 'animation--end');
-  element.appendChild(divEl);
-  const height = divEl.clientHeight;
-  divEl.style.left = `${x - height / 2}px`;
-  divEl.style.top = `${y - height / 2}px`;
-};
-document.addEventListener('mousemove', (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
-
-  createElPrincipal(bodyEl, x, y);
-  createElMiddle(bodyEl, x, y);
-  createElend(bodyEl, x, y);
+bodyEl.addEventListener('mousemove', (event) => {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  createElementTrail(bodyEl, x, y);
 });
