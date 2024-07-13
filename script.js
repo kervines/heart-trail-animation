439398aea677f60c794917914749d4a8d36df23e
kervines <kervinzero@gmail.com>
2024-07-13 16:32:42 -0300

    update: adicionado funcionalidade color random

--- script.js
+++ script.js
@@ -2,25 +2,22 @@ const bodyEl = document.querySelector('body');
 
 const createElementTrail = (element, x, y) => {
   const spanEl = document.createElement('span');
+  const colorRandom = parseInt(Math.random() * 360 + 1);
   const size = Math.random() * 100;
+
   spanEl.style.width = `${size}px`;
   spanEl.style.height = `${size}px`;
+
+  spanEl.style.filter = `hue-rotate(${colorRandom}deg)`;
 
   element.appendChild(spanEl);
   spanEl.style.left = `${x}px`;
   spanEl.style.top = `${y}px`;
 
-  colorRandom(spanEl.style.filter);
-
   setTimeout(() => {
     spanEl.remove();
   }, 3000);
 };
-
-function colorRandom(element) {
-  const colorRandom = Math.round(Math.random() * 360);
-  element = `hue-rotate(${colorRandom}deg)`;
-}
 
 bodyEl.addEventListener('mousemove', ({ offsetX, offsetY }) => {
   createElementTrail(bodyEl, offsetX, offsetY);
